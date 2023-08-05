b00bc89e28fd41ca79b44415e377b7c3877ce9db
ssh <victoria9981@naver.com>
2023-08-05 23:19:54 +0900

    8월 5일자 커밋/ 온보딩화면 구현완료

--- app/src/main/java/com/example/nongglenonggle/view/SplashActivity.kt
+++ app/src/main/java/com/example/nongglenonggle/view/SplashActivity.kt
@@ -7,17 +7,32 @@ import android.os.Handler
 import android.os.Looper
 import com.example.nongglenonggle.MainActivity
 import com.example.nongglenonggle.R
+import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
+
+    //coroutinescope 객체 생성
+    //dispatcher.main 사용해서 코루틴이 메인(ui) 쓰레드에서 실행되도록 설정한다
+    val activityScope= CoroutineScope(Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        //일정시간 지연이후 실행하기 위한 코드
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+        //코루틴을 시작하는 블록
+        activityScope.launch {
+            delay(3000)
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        },500)//시간 0.5초 이후 실행
+        }
+    }
+
+    //액티비티가 일시정지 상태로 전환될때 호출되는 생명주기 함수
+    //현재 실행중인 코루틴을 취소
+    //액티비티 일시정지되는 동안 불필요한 코루틴 실행 방지
+    override fun onPause()
+    {
+        activityScope.cancel()
+        super.onPause()
     }
 }
