948b93875e5a19fcb52341374c6060aa47844191
songseunghui <victoria9981@naver.com>
2023-10-25 00:16:13 +0900

    [Fix] 공고글 dataclass 옮기기 완료

--- app/src/main/java/com/example/nongglenonggle/domain/entity/Model.kt
+++ app/src/main/java/com/example/nongglenonggle/domain/entity/Model.kt
@@ -5,28 +5,6 @@ import android.net.Uri
 class Model {
     data class ImageEntity(val uri: Uri)
 
-    data class NoticeContent(
-        val imageUrl : String,
-        val pay : List<String>,
-        val recruitPeriod : Map<String,Any>,
-        val recruitNum : String,
-        val recruitAge : List<String>,
-        val recruitGender:String,
-        val career : String,
-        val qualification : Map<String,Any>,
-        val workPeriod:List<String>,
-        val workType:String,
-        val workDay : Map<String,Any>,
-        val workTime:Map<String,Any>,
-        val title:String,
-        val categoryItem:List<String>,
-        val firstAddress:String,
-        val finalAddress:String,
-        val workDetailInfo:String,
-        val stayInfo:String,
-        val mealInfo:String,
-        val specialInfo:String
-    )
     data class ResumeSummary(
         val title : String,
         val date : String,
--- app/src/main/java/com/example/nongglenonggle/domain/entity/NoticeContent.kt
+++ app/src/main/java/com/example/nongglenonggle/domain/entity/NoticeContent.kt
@@ -1,3 +1,24 @@
 package com.example.nongglenonggle.domain.entity
 
-data class NoticeContent()
+data class NoticeContent(
+    val imageUrl : String,
+    val pay : List<String>,
+    val recruitPeriod : Map<String,Any>,
+    val recruitNum : String,
+    val recruitAge : List<String>,
+    val recruitGender:String,
+    val career : String,
+    val qualification : Map<String,Any>,
+    val workPeriod:List<String>,
+    val workType:String,
+    val workDay : Map<String,Any>,
+    val workTime:Map<String,Any>,
+    val title:String,
+    val categoryItem:List<String>,
+    val firstAddress:String,
+    val finalAddress:String,
+    val workDetailInfo:String,
+    val stayInfo:String,
+    val mealInfo:String,
+    val specialInfo:String
+)
