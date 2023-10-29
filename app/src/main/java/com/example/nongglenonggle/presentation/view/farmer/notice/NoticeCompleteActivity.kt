c51f6d4d901de5f5f51dbc8f92064c555953f1d4
songseunghui <victoria9981@naver.com>
2023-10-29 18:01:52 +0900

    [Design] 공고글 완료 화면 디자인 및 컬러코드 변경

---
+++ app/src/main/java/com/example/nongglenonggle/presentation/view/farmer/notice/NoticeCompleteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nongglenonggle.presentation.view.farmer.notice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nongglenonggle.R
+
+class NoticeCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notice_complete)
+    }
+}
