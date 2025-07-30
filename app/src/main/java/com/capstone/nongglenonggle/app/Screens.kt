2ce8aa1cf079da6074181d52baa96d0aee479345
seunghee <victoria9981@naver.com>
2025-07-31 01:02:11 +0900

    feat: 설명 주석 수정

--- app/src/main/java/com/capstone/nongglenonggle/app/Screens.kt
+++ app/src/main/java/com/capstone/nongglenonggle/app/Screens.kt
@@ -1,8 +1,8 @@
 package com.capstone.nongglenonggle.app
 
-//navigation을 구현하다보면 제일 난감한게 어디 경로로 가는지 확실히 파악할 수 없는것이다
-//gorouter과 같은 방식으로 요즘엔 경로를 문자열로 지정하여 개발자가 명확히 어느경로로 갈수있는지
-//파악 가능
+//Jetpack Compose Navigation을 사용할 때 경로가 문자열로 관리되기 때문에
+//어디로 이동하는지 명확하지 않은 경우가 많음
+//이를 방지하기 위해 컴파일 타임에 유효한 경로를 사용할 수 있도록 모든 경로 타입을 정의하여 구조화함
 sealed class Screens(val route: String) {
     object StartNonggleScreen: Screens("StartNonggle")
     object Signup: Screens("SignUp") {
