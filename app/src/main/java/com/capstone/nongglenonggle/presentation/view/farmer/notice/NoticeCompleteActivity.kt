73e0dad2a099d4674231e0a7815ce5b53a348995
seunghee <victoria9981@naver.com>
2025-08-18 23:02:53 +0900

    feat: import 구문 정리

--- app/src/main/java/com/capstone/nongglenonggle/presentation/view/farmer/notice/NoticeCompleteActivity.kt
+++ app/src/main/java/com/capstone/nongglenonggle/presentation/view/farmer/notice/NoticeCompleteActivity.kt
@@ -2,7 +2,6 @@ package com.capstone.nongglenonggle.presentation.view.farmer.notice
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
