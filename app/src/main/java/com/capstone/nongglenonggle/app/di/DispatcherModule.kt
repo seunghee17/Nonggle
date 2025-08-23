24b6741ba50c1842e5c755cacd0e5ebfa2bb9fc9
seunghee <victoria9981@naver.com>
2025-08-23 13:06:19 +0900

    feat: dispatcher hilt를 통해 주입받는 구조 구축을 위해 module생성 및 한정자 정의

---
+++ app/src/main/java/com/capstone/nongglenonggle/app/di/DispatcherModule.kt
@@ -0,0 +1,29 @@
+package com.capstone.nongglenonggle.app.di
+
+import com.capstone.nongglenonggle.domain.qualifiers.DefaultDispatcher
+import com.capstone.nongglenonggle.domain.qualifiers.IoDispatcher
+import com.capstone.nongglenonggle.domain.qualifiers.MainDispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+
+    @Provides
+    @DefaultDispatcher
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @Provides
+    @IoDispatcher
+    fun provideIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @MainDispatcher
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
