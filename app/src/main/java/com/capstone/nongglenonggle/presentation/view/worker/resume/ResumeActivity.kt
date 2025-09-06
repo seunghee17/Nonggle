ca35bd3e32ef1e99cb23a8c8895519368add040b
seunghee <victoria9981@naver.com>
2025-09-06 10:12:40 +0900

    feat: 불필요 파일 및 코드 삭제

--- app/src/main/java/com/capstone/nongglenonggle/presentation/view/worker/resume/ResumeActivity.kt
+++ app/src/main/java/com/capstone/nongglenonggle/presentation/view/worker/resume/ResumeActivity.kt
@@ -1,66 +1,18 @@
 package com.capstone.nongglenonggle.presentation.view.worker.resume
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.viewpager2.widget.ViewPager2
-import com.capstone.nongglenonggle.R
 import com.capstone.nongglenonggle.app.ResumeNavHost
-import com.capstone.nongglenonggle.databinding.ActivityResumeBinding
-import com.capstone.nongglenonggle.presentation.base.BaseActivity
-import com.capstone.nongglenonggle.presentation.view.adapter.ViewPager2Adapter
-import com.capstone.nongglenonggle.presentation.view.dialog.OutDirectionDialogFragment
-import com.capstone.nongglenonggle.presentation.view.splash.SpalashScreen
-import com.capstone.nongglenonggle.presentation.view.splash.SplashViewModel
-import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ResumeActivity : BaseActivity<ActivityResumeBinding>(R.layout.activity_resume) {
-    private val viewModel: ResumeViewModel by viewModels()
+class ResumeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding.viewModel = viewModel
-//        binding.lifecycleOwner = this
-//
-//        binding.backbtn.setOnClickListener{
-//            showDialog()
-//        }
-//        initViewPager()
+
         setContent {
             ResumeNavHost()
         }
-    }
-
-    private fun initViewPager(){
-        var viewPager2Adapter = ViewPager2Adapter(this)
-        viewPager2Adapter.addfragment(ResumeAFragment())
-        viewPager2Adapter.addfragment(ResumeBFragment())
-        viewPager2Adapter.addfragment(ResumeCFragment())
-        viewPager2Adapter.addfragment(ResumeDFragment())
-
-        binding.viewPager.apply {
-            adapter = viewPager2Adapter
-            registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
-                override fun onPageSelected(position: Int) {
-                    super.onPageSelected(position)
-                }
-            })
-        }
-        TabLayoutMediator(binding.tablayout, binding.viewPager){
-            tab,position->
-            when(position){
-                0 -> tab.text = "기본정보"
-                1-> tab.text = "경력사항"
-                2-> tab.text = "자기소개"
-                3-> tab.text = "희망조건"
-            }
-        }.attach()
-    }
-
-    private fun showDialog(){
-        val dialog = OutDirectionDialogFragment()
-        dialog.show(supportFragmentManager,"dialog")
     }
 }
