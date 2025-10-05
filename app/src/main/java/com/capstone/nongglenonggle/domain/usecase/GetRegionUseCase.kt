e9ccac8246ef7200c331326e2df8875f510dd6dd
seunghee <victoria9981@naver.com>
2025-10-05 21:41:17 +0900

    feat: 상위 패키지명 변경

--- app/src/main/java/com/capstone/nongglenonggle/domain/usecase/GetRegionUseCase.kt
+++ app/src/main/java/com/capstone/nongglenonggle/domain/usecase/GetRegionUseCase.kt
@@ -1,7 +1,7 @@
 package com.capstone.nongglenonggle.domain.usecase
 
 import com.capstone.nongglenonggle.data.model.remote_model.RegionModel
-import com.capstone.nongglenonggle.data.remote_datasource.ApiResult
+import com.capstone.nongglenonggle.data.network.ApiResult
 import com.capstone.nongglenonggle.domain.qualifiers.IoDispatcher
 import com.capstone.nongglenonggle.domain.repository.RemoteDataRepository
 import kotlinx.coroutines.CoroutineDispatcher
