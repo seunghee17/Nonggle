fc0cd8623d40539832e5309839e31b9569987e73
seunghee <victoria9981@naver.com>
2025-10-06 12:06:09 +0900

    feat: 디렉토리 이동

--- app/src/main/java/com/capstone/nongglenonggle/presentation/view/worker/resume/component/NonggleDropDown.kt
+++ app/src/main/java/com/capstone/nongglenonggle/presentation/view/worker/resume/component/NonggleDropDown.kt
@@ -1,4 +1,4 @@
-package com.capstone.nongglenonggle.presentation.view.worker.resume.parent_component
+package com.capstone.nongglenonggle.presentation.view.worker.resume.component
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
