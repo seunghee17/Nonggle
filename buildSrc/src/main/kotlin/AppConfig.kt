005012e714adba3e3f95b70000709f899f3add59
송승희 <victoria9981@fasoo.com>
2025-11-24 16:15:34 +0900

    feat: 버전관리 카탈로그 추가

---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,8 @@
+object AppConfig {
+    const val compileSdk = 36
+    const val buildToolsVersion = "30.0.2"
+    const val minSdk = 24
+    const val targetSdk = 31
+    const val versionCode = 1
+    const val versionName = "0.0.1"
+}
