039212283ce21f503bba3f4e9261851e61e65edb
송승희 <victoria9981@fasoo.com>
2025-11-24 13:12:08 +0900

    feat: compile sdk 36 업그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ kotlin {
 
 android {
     namespace = "com.capstone.nongglenonggle"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.capstone.nongglenonggle"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
