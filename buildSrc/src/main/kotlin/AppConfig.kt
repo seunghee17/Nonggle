88797c6920062a962ffb2e09445826ffb5f481b3
seunghee <victoria9981@naver.com>
2025-11-26 23:59:47 +0900

    feat: 멀티모듈용 AppConfig 정의

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -1,8 +1,7 @@
-//object AppConfig {
-//    const val compileSdk = 36
-//    const val buildToolsVersion = "30.0.2"
-//    const val minSdk = 24
-//    const val targetSdk = 31
-//    const val versionCode = 1
-//    const val versionName = "0.0.1"
-//}
+object AppConfig {
+    const val compileSdk = 36
+    const val minSdk = 26
+    const val targetSdk = 36
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
