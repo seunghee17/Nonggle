85a2ee96406c7f55ef35354e73d7bc2dffcd29d1
seunghee <victoria9981@naver.com>
2026-01-04 18:29:35 +0900

    feat:안드로이드 compose 관련 빌드로직 정의하기 위해 AndroidCompose 유틸 함수 정의

---
+++ build-logic/convention/src/main/java/com/example/convention/AndroidCompose.kt
@@ -0,0 +1,26 @@
+package com.example.convention
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+internal fun Project.configureAndroidCompose(
+    commonExtension: CommonExtension<*, *, *, *, *, *>
+) {
+    commonExtension.run {
+        buildFeatures {
+            compose = true
+        }
+
+        composeOptions {
+            kotlinCompilerExtensionVersion = libs.findVersion("composeCompiler").get().toString()
+        }
+
+        dependencies {
+            val bom = libs.findLibrary("androidx.compose.bom").get()
+            add("implementation", platform(bom))
+            add("androidTestImplementation", platform(bom))
+            add("debugImplementation", libs.findLibrary("androidx.ui.tooling.preview").get())
+        }
+    }
+}
